6155b1a92f0f85c7da93696accb81e20cca638cf
Manu Guilbert <manu.guilbert9@gmail.com>
2025-08-31 14:37:24 +0000

    Je ne suis pas sûr qu'on soit connecté à la base de données Firebase. Je

---
+++ src/lib/firebase.ts
@@ -0,0 +1,20 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDKofhni6U_izWTd7bdGayfW2gVG4tCsAc",
+  authDomain: "skillfiesta.firebaseapp.com",
+  projectId: "skillfiesta",
+  storageBucket: "skillfiesta.firebasestorage.app",
+  messagingSenderId: "294782662108",
+  appId: "1:294782662108:web:5af404a22d68a637d04b19"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+
+export { app, db };
