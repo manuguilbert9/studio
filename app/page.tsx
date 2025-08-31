5207577c9f2e4ecee418a1d28f25f17243eaa034
Manu Guilbert <manu.guilbert9@gmail.com>
2025-08-31 13:51:23 +0000

    Remplace changer d'utilisateur par déconnexion

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { useState, useEffect, FormEvent } from 'react';
 import Link from 'next/link';
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from '@/components/ui/card';
 import { Logo } from '@/components/logo';
-import { Book, Users, UserCog, ArrowRight } from 'lucide-react';
+import { Book, Users, LogOut, ArrowRight } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
@@ -135,8 +135,8 @@ export default function ModeSelectionPage() {
       </div>
         <div className="mt-12">
             <Button onClick={handleUserChange} variant="outline" size="lg">
-                <UserCog className="mr-2" />
-                Changer d'utilisateur
+                <LogOut className="mr-2" />
+                Déconnexion
             </Button>
         </div>
     </main>
