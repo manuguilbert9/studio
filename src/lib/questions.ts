9b705168935fcf32f35a55609d4ef4d8361affd9
Manu Guilbert <manu.guilbert9@gmail.com>
2025-09-03 21:54:34 +0000

    Je suis allé chercher dans une version précédente de l'application dans

--- src/lib/questions.ts
+++ src/lib/questions.ts
@@ -155,19 +155,19 @@ function generateTimeQuestion(settings: TimeSettings): Question {
 }
 
 export const currency: CurrencyItem[] = [
-  { name: '1 cent', value: 1, image: '/1cent.png', hint: 'pièce 1 centime' },
-  { name: '2 cents', value: 2, image: '/2cents.png', hint: 'pièce 2 centimes' },
-  { name: '5 cents', value: 5, image: '/5cents.png', hint: 'pièce 5 centimes' },
-  { name: '10 cents', value: 10, image: '/10cents.png', hint: 'pièce 10 centimes' },
-  { name: '20 cents', value: 20, image: '/20cents.png', hint: 'pièce 20 centimes' },
-  { name: '50 cents', value: 50, image: '/50cents.png', hint: 'pièce 50 centimes' },
-  { name: '1 euro', value: 100, image: '/1euro.png', hint: 'pièce 1 euro' },
-  { name: '2 euros', value: 200, image: '/2euros.png', hint: 'pièce 2 euros' },
-  { name: '5 euros', value: 500, image: '/5euros.png', hint: 'billet 5 euros' },
-  { name: '10 euros', value: 1000, image: '/10euros.png', hint: 'billet 10 euros' },
-  { name: '20 euros', value: 2000, image: '/20euros.png', hint: 'billet 20 euros' },
-  { name: '50 euros', value: 5000, image: '/50euros.png', hint: 'billet 50 euros' },
-  { name: '100 euros', value: 10000, image: '/100euros.png', hint: 'billet 100 euros' },
+  { name: '1 cent', value: 1, image: '/monnaie/1cent.png', hint: 'pièce 1 centime' },
+  { name: '2 cents', value: 2, image: '/monnaie/2cents.png', hint: 'pièce 2 centimes' },
+  { name: '5 cents', value: 5, image: '/monnaie/5cents.png', hint: 'pièce 5 centimes' },
+  { name: '10 cents', value: 10, image: '/monnaie/10cents.png', hint: 'pièce 10 centimes' },
+  { name: '20 cents', value: 20, image: '/monnaie/20cents.png', hint: 'pièce 20 centimes' },
+  { name: '50 cents', value: 50, image: '/monnaie/50cents.png', hint: 'pièce 50 centimes' },
+  { name: '1 euro', value: 100, image: '/monnaie/1euro.png', hint: 'pièce 1 euro' },
+  { name: '2 euros', value: 200, image: '/monnaie/2euros.png', hint: 'pièce 2 euros' },
+  { name: '5 euros', value: 500, image: '/monnaie/5euros.png', hint: 'billet 5 euros' },
+  { name: '10 euros', value: 1000, image: '/monnaie/10euros.png', hint: 'billet 10 euros' },
+  { name: '20 euros', value: 2000, image: '/monnaie/20euros.png', hint: 'billet 20 euros' },
+  { name: '50 euros', value: 5000, image: '/monnaie/50euros.png', hint: 'billet 50 euros' },
+  { name: '100 euros', value: 10000, image: '/monnaie/100euros.png', hint: 'billet 100 euros' },
 ];
 
 export const formatCurrency = (value: number) => {
@@ -418,7 +418,7 @@ function generateCalculationQuestion(settings: CalculationSettings): Question {
                 num2 = Math.floor(Math.random() * num1); 
             }
             question = `${num1} - ${num2} = ?`;
-            answer = num1 - num2;
+            answer = num1 + num2;
             break;
 
         case 'x':
