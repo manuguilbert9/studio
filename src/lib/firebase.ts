47732e66f039379d96afb725550cd6494d78fa0e
Manu Guilbert <manu.guilbert9@gmail.com>
2025-09-03 16:54:13 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,16 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDKofhni6U_izWTd7bdGayfW2gVG4tCsAc",
-  authDomain: "skillfiesta.firebaseapp.com",
-  projectId: "skillfiesta",
-  storageBucket: "skillfiesta.firebasestorage.app",
-  messagingSenderId: "294782662108",
-  appId: "1:294782662108:web:5af404a22d68a637d04b19"
+  "projectId": "classemagique2",
+  "appId": "1:461454261743:web:81c2fe779d83fb487fca1a",
+  "storageBucket": "classemagique2.firebasestorage.app",
+  "apiKey": "AIzaSyCWxtFBSdvAGiJN06WxRLFtFZ0xGPe9E9Q",
+  "authDomain": "classemagique2.firebaseapp.com",
+  "messagingSenderId": "461454261743"
 };
 
 // Initialize Firebase
